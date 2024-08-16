67ba043f4cc0e2ce72f878d565170dace966133e
Branislav Guduric <branislavguduricns@gmail.com>
2024-08-16 15:30:37 +0200

    Fixed CSS. Added on-hover for navigation.

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 @app.route("/index")
-def home():
+def index():
     return render_template("index.html")
 
 @app.route("/about")
-def about_me():
+def about():
     return render_template("about.html")
 
 @app.route("/contact")
