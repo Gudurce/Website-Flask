4b1e49bda4580fd82d722e8f7053e1df86c41bf8
Branislav Guduric <branislavguduricns@gmail.com>
2024-08-16 11:55:06 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/index")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about_me():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/expertise")
+def expertise():
+    return render_template("expertise.html")
+
+if __name__ == "__main__":
+    app.run()
